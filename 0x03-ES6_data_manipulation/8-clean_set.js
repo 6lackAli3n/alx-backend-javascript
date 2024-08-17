0bc53bbbc3ba6497233b71e2a700cb505e3f9e95
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-08-17 02:17:12 +0100

    all

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,8 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
+  if (!(set instanceof Set) || typeof startString !== 'string') return '';
+
   return [...set]
-    .filter((el) => el.startsWith(startString))
+    .filter((el) => typeof el === 'string' && el.startsWith(startString))
     .map((el) => el.slice(startString.length))
     .join('-');
 }
